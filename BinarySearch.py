e9245935996fa0c039c796487f22d7763934a95f
Frederik Sherman <sherfr94@gmail.com>
2018-02-28 15:15:37 +0200

    PEP8 pass

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,10 +1,10 @@
 # Returns index of item in arr if present, else -1
-def binarySearch(arr, left, right, item):
+def binary_search(arr, left, right, item):
     # Check base case
     if right >= left:
 
-        mid =  int(left + (right - left) / 2)
-        #print("mid:", mid, "l:", left, "r:", right)
+        mid = int(left + (right - left) / 2)
+        # print("mid:", mid, "l:", left, "r:", right)
 
         # If element is present at the middle itself
         if arr[mid] == item:
@@ -13,12 +13,12 @@ def binarySearch(arr, left, right, item):
         # If element is smaller than mid, then it 
         # can only be present in left subarray
         elif arr[mid] > item:
-            return binarySearch(arr, left, mid - 1, item)
+            return binary_search(arr, left, mid - 1, item)
 
         # Else the element can only be present 
         # in right subarray
         else:
-            return binarySearch(arr, mid + 1, right, item)
+            return binary_search(arr, mid + 1, right, item)
 
     else:
         # Element is not present in the array
@@ -26,12 +26,12 @@ def binarySearch(arr, left, right, item):
 
 
 # Test array
-arr = [2, 3, 4, 10, 40]
-item = 40
-print("arr:", arr, "item:", item)
+arr1 = [2, 3, 4, 10, 40]
+item1 = 40
+print("arr:", arr1, "item:", item1)
 
 # Function call
-result = binarySearch(arr, 0, len(arr) - 1, item)
+result = binary_search(arr1, 0, len(arr1) - 1, item1)
 
 if result != -1:
     print("item found at index", result)
